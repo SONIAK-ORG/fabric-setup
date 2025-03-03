8922bddb69d1594218b84bcb8ce52dd09604ccb7
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2025-03-03 19:07:46 +0000

    Update main.tf

--- module/capacity/main.tf
+++ module/capacity/main.tf
@@ -1,12 +1,11 @@
-# Specify required providers
 terraform {
   required_providers {
     azapi = {
-      source  = "azure/azapi" # Ensure this matches the provider source in the root module
+      source  = "azure/azapi"
       version = "~> 2.2.0"
     }
     azurerm = {
-      source  = "hashicorp/azurerm" # Ensure this matches the provider source in the root module
+      source  = "hashicorp/azurerm"
       version = "~> 4.16.0"
     }
   }
@@ -16,11 +15,10 @@ terraform {
   }
 }
 
-
 provider "azurerm" {
-  features {}
-  use_oidc = true
-  use_cli = false
+  features        = {}
+  use_oidc        = true
+  use_cli         = false
   subscription_id = "229f8a90-d75b-41db-ae79-90cdc72a0d11"
 }
 
@@ -29,21 +27,12 @@ provider "azapi" {
   use_cli  = false
 }
 
-
-
-
 locals {
-  # Define constant admin members that should always be included
   default_admin_members = []
-
-  # Parse YAML file relative to this module
-  config = yamldecode(file("${path.module}/../../variables.yaml"))
-
-  # Safely extract capacities, defaulting to empty map
-  capacities = try(local.config.capacities, {})
+  capacities = var.capacities
+  workspaces = var.workspaces
 }
 
-# Dynamically retrieve resource group IDs
 data "azurerm_resource_group" "fab" {
   for_each = local.capacities
   name     = each.value.resource_group_name
@@ -61,7 +50,10 @@ resource "azapi_resource" "fab_capacity" {
   body = {
     properties = {
       administration = {
-        members = concat(local.default_admin_members, try(coalesce(each.value.administration.members, []), []))
+        members = concat(
+          local.default_admin_members,
+          try(coalesce(each.value.administration.members, []), [])
+        )
       }
     }
     sku = {
@@ -72,3 +64,4 @@ resource "azapi_resource" "fab_capacity" {
 
   tags = each.value.tags
 }
+
